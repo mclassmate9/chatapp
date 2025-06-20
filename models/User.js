652db38d3bd2dadcd4caa9883985ac4259a8dcd3
mclassmate9 <mclassmate9@gmail.com>
2025-06-19 23:18:42 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
-// models/User.js
+const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
   userId: { type: String, unique: true },
   email: { type: String, unique: true },
   password: String,
-  contacts: [{ type: String }] // store contact userIds
+  contacts: [{ type: String }] // 👈 ADD THIS LINE
 });
-
 
 module.exports = mongoose.model('User', userSchema);

6d042c1b89a056cab17df544011b8607a6f9811e
mclassmate9 <mclassmate9@gmail.com>
2025-06-20 16:20:25 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
-const router = express.Router();
 
 // Register route
 router.post('/register', async (req, res) => {

5b40045ffdc7ace5ccaa42cd2c14b7d44a89e7bd
mclassmate9 <mclassmate9@gmail.com>
2025-06-20 23:29:56 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -9,7 +9,17 @@ const userSchema = new mongoose.Schema({
   userId: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  contacts: [contactSchema]
+  
+contacts: [
+  {
+    userId: String,
+    status: {
+      type: String,
+      enum: ['pending', 'approved'],
+      default: 'pending',
+    },
+  }
+],
 });
 
 module.exports = mongoose.model('User', userSchema);

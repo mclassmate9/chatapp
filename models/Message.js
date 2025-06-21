83d058798fbed101c8957f4c2c6398ef0facfc25
mclassmate9 <mclassmate9@gmail.com>
2025-06-21 16:01:51 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,1 +1,16 @@
-ujhgiugiuyguh
+// models/Message.js
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  user: String,       // sender's userId
+  to: String,         // receiver's userId
+  text: String,
+  time: { type: Date, default: Date.now, expires: 345600 }, // auto-delete after 4 days
+  status: {
+    type: String,
+    enum: ['sent', 'delivered', 'seen'],
+    default: 'sent'
+  }
+});
+
+module.exports = mongoose.model('Message', messageSchema);

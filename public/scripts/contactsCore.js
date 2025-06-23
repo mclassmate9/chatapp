d6447c9fd2241a2118270fe646e55ac999be1668
mclassmate9 <mclassmate9@gmail.com>
2025-06-22 23:37:47 -0700

    Update contactsCore.js

--- public/scripts/contactsCore.js
+++ public/scripts/contactsCore.js
@@ -13,7 +13,7 @@ export async function fetchAllContacts() {
 }
 
 export async function sendContactRequest(contactId) {
-  const res = await fetch('/contacts/list', {
+  const res = await fetch('/contacts/request', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ contactId }),
